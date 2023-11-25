ca2d61533b9163cee0c515325fbd1cccb92ec487
AleCampos <alemdcamposs@gmail.com>
2023-11-25 16:37:44 -0300

    number

---
+++ 2-FundamentosTypeScript/index.js
@@ -0,0 +1,9 @@
+var x = 10;
+console.log(x);
+x = 'alexandre';
+console.log(x);
+var y = 30.85646;
+console.log(typeof y);
+console.log(y.toPrecision(4));
+var number2 = 30;
+console.log(typeof number2);
---
+++ 2-FundamentosTypeScript/index.ts
@@ -0,0 +1,12 @@
+let x: number = 10;
+console.log(x);
+
+x = 'alexandre';
+console.log(x);
+
+let y: number = 30.85646;
+console.log(typeof y);
+console.log(y.toPrecision(4));
+
+let number2: number = 30;
+console.log(typeof number2);
