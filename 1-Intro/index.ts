f82246f461a5da6659e89101bf0a581f34eafd82
AleCampos <alemdcamposs@gmail.com>
2023-11-25 16:04:53 -0300

    Intro

---
+++ 1-Intro/index.ts
@@ -0,0 +1,22 @@
+const nome = 'alexadre';
+const idade = 30;
+const boolean = true;
+
+function test(n: string): void {
+  console.log('Olá ' + n);
+}
+
+test(nome);
+
+// test(x);
+
+// 1-Desafio
+
+const numOne = 5;
+const numTwon = 10;
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+console.log(sum(numOne, numTwon));
