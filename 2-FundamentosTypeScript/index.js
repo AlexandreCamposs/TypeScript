98a30aba7808ef4d4dcd8cde3b6b68b8b02e95c7
AleCampos <alemdcamposs@gmail.com>
2023-11-25 16:45:53 -0300

    string

--- 2-FundamentosTypeScript/index.js
+++ 2-FundamentosTypeScript/index.js
@@ -1,3 +1,4 @@
+//1-Number
 var x = 10;
 console.log(x);
 x = 'alexandre';
@@ -7,3 +8,14 @@ console.log(typeof y);
 console.log(y.toPrecision(4));
 var number2 = 30;
 console.log(typeof number2);
+//2-String
+var firstName = 'alexandre';
+console.log(firstName.toUpperCase());
+var lastName = 'campos';
+console.log(lastName.toUpperCase());
+var myName = firstName + ' ' + lastName;
+console.log(myName);
+var idade;
+console.log(idade);
+idade = 30;
+console.log('idade: ' + idade);
--- 2-FundamentosTypeScript/index.ts
+++ 2-FundamentosTypeScript/index.ts
@@ -1,3 +1,4 @@
+//1-Number
 let x: number = 10;
 console.log(x);
 
@@ -10,3 +11,22 @@ console.log(y.toPrecision(4));
 
 let number2: number = 30;
 console.log(typeof number2);
+
+//2-String
+const firstName: string = 'alexandre';
+console.log(firstName.toUpperCase());
+
+const lastName: string = 'campos';
+console.log(lastName.toUpperCase());
+
+const myName: string = firstName + ' ' + lastName;
+
+console.log(myName);
+
+let idade: number;
+
+console.log(idade);
+
+idade = 30;
+
+console.log('idade: ' + idade);
